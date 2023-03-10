e47cefea671a33f5a6f0be0131a4242249c734a5
Erwin Darsono <erwindarsono08@gmail.com>
2023-03-11 00:12:22 +0700

    testing again

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function playRound(playerSelection, computerSelection) {
     } else if (computerSelection === "scissor") {
       return "player wins";
     }
-  } else if (playerChoice === "papera") {
+  } else if (playerChoice === "paper") {
     if (computerSelection === "rock") {
       return "player wins";
     } else if (computerSelection === "paper") {
